6fffe11c9439e17715130d2ee3c456af97f027f8
amorphobia <pengxuesong.cn@gmail.com>
2022-04-24 16:21:29 +0800

    first commit

---
+++ src/posh-cd.psd1
@@ -0,0 +1,31 @@
+#
+# Module manifest for module 'posh-cd'
+#
+
+@{
+
+RootModule = 'posh-cd.psm1'
+ModuleVersion = '0.0.1'
+GUID = 'cecff5d4-50d6-4ff9-ba22-e8b083276d8c'
+Author = 'amorphobia'
+Description = 'A bash style "cd" to replace Set-Location'
+PowerShellVersion = '5.0'
+
+FunctionsToExport = @('Set-Directory')
+CmdletsToExport = @()
+VariablesToExport = '*'
+AliasesToExport = @('cd')
+
+PrivateData = @{
+
+    PSData = @{
+
+        Tags = @('bash', 'cd')
+        LicenseUri = 'https://github.com/amorphobia/posh-cd/blob/master/LICENSE'
+
+    }
+
+}
+
+}
+
---
+++ src/posh-cd.psm1
@@ -0,0 +1,28 @@
+function Set-Directory {
+    param([string] $Path)
+
+    if ([string]::IsNullOrEmpty($Path)) {
+        $TargetPath = '~'
+    } elseif ($Path -eq '-') {
+        $TargetPath = $env:OLDPWD
+    } else {
+        $TargetPath = $Path
+    }
+
+    if ([string]::IsNullOrEmpty($TargetPath)) {
+        Write-Error -Message '$env:OLDPWD not set' -ErrorAction Stop
+    } else {
+        $env:OLDPWD = $(Get-Location).Path
+        Set-Location $TargetPath
+    }
+    if ($Path -eq '-') {
+        Write-Output $TargetPath
+    }
+}
+
+while (Test-Path Alias:cd) {
+    Remove-Item Alias:cd
+}
+
+Set-Alias -Name cd -Value Set-Directory
+Export-ModuleMember -Alias cd -Function Set-Directory
